bed268fe475222459d5d340225df440b800e2a3f
Abdukulov Talgat <abdukulov.talgat@gmail.com>
2022-08-03 16:46:58 +0500

    Добавляет планшетную версию header

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector('.header__burger');
 const html = document.documentElement;
 
-burger.addEventListener('click', () => {
+burger.addEventListener('click', (evt) => {
+    evt.preventDefault();
     html.classList.toggle('menu-open');
 });
